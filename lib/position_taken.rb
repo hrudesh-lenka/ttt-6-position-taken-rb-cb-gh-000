645a3e0626c0bb67b19e300a00ed75febf657dcd
Hrudesh Lenka <hrudesh2010@gmail.com>
2020-06-23 16:40:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if (board[position] ==  " " || board[position] == "" || board[position] == nil)	  # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    return false
+  else
+    return true
+  end
+end
+
+position_taken?(board, index)
