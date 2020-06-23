e157cc025521e3e165ed472538be4d3489bb985b
Hrudesh Lenka <hrudesh2010@gmail.com>
2020-06-23 16:44:25 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
-  if (board[position] ==  " " || board[position] == "" || board[position] == nil)	  # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
     return false
   else
     return true
